83f2e98f3d1344a64b3e540d6c2c22a542aede46
Plaswalro <feenixbox@gmail.com>
2019-01-11 16:00:05 +0300

    110119

--- BOI.java
+++ BOI.java
@@ -1,40 +1,47 @@
 /*
 TODO:
-1. SAVE/LOAD A PATTERN (MENU BAR -> FILE -> NEW, SAVE, LOAD)
-2. ANOTHER FIELD FOR PLAYER SHIPS
-3. MANUAL SETUP
-4. AI
-5. MAYBE
+1. RESULT/HIGHSCORE FRAME
+2. HIGHLIGHT FOR KILLS
 */
 
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 
 public class BOI {
 	static final int MAP_X = 10;
 	static final int MAP_Y = 10;
 	static final int SHIP_N = 4;
-	static ArrayList<Ship> shipList = new ArrayList<Ship>();
+	static ArrayList<Ship> shipList;
+	static ArrayList<Tile> tileList;
+	static boolean gameInactive;
+	static int guessCount;
+
+	ArrayList<Integer> highScore = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
 		BOI game = new BOI();
-
-		Generator.makeMap(MAP_X, MAP_Y, SHIP_N);
-		System.out.println("Generator.MakeMap() successful.");
-		shipList = Generator.getShipList();
-		game.makeGui(shipList);
+		game.setUpGame();
+		game.makeGui();
 	}
 
-	public void makeGui(ArrayList<Ship> shipList) {
+	public void setUpGame() {
+		Generator.makeMap(MAP_X, MAP_Y, SHIP_N);
+		shipList = Generator.getShipList();
+	}
 
-		JFrame f = new JFrame("BOI!");
+	public void makeGui() {
+		JFrame f;
+		f = new JFrame("BOI!");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setSize(600,600);
+		f.setLocationRelativeTo(null);
+		f.setUndecorated(true);
 		f.setResizable(false);
-		
-		GridLayout grid = new GridLayout(10,10);
+	
+		GridLayout grid = new GridLayout(MAP_X, MAP_Y);
 		grid.setVgap(4);
 		grid.setHgap(4);
 
@@ -42,33 +49,81 @@ public class BOI {
 		background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 		background.setLayout(grid);
 
-		TileListener tl = new TileListener();
-		
-		for(int i = 0; i < 10; i++) {
-			for(int j = 0; j < 10; j++) {
-				Tile field = new Tile(i, j);
+		GuessListener tl = new GuessListener();
+
+		f.getContentPane().add(BorderLayout.CENTER, background);
+		JMenuBar menuBar = new JMenuBar();
+		JMenuItem restartMenuItem = new JMenuItem("Restart");
+		JMenuItem highScoresMenuItem = new JMenuItem("High Scores");
+
+		restartMenuItem.addActionListener(new RestartListener());
+		highScoresMenuItem.addActionListener(new highScoresListener());
+
+		menuBar.add(restartMenuItem);
+		menuBar.add(highScoresMenuItem);
+
+		f.setJMenuBar(menuBar);
+
+		tileList = new ArrayList<Tile>();
+
+		for(int j = 0; j < MAP_Y; j++) {
+			for(int i = 0; i < MAP_X; i++) {
+				Tile tile = new Tile(i, j);
 				for(Ship ship: shipList) {
-					if(ship.isIn(field.XY)) {
-						field.isShip();
+					if(ship.cells.contains(tile.location)) {
+						tile.hasShip();
+						break;
 					}
 				}
-				field.addMouseListener(tl);
-				background.add(field);
+				tileList.add(tile);
+				tile.addMouseListener(tl);
+				background.add(tile);
 			}//j
 		}//i
 
-		f.getContentPane().add(BorderLayout.CENTER, background);
 		f.setVisible(true);
+
 	}//method
 
-	class TileListener extends MouseAdapter implements MouseListener {
-		public void mouseReleased(MouseEvent event) {
-			Tile t = (Tile)(event.getSource());
-			if(t.isChecked == false) {
-				t.isChecked();
-				t.repaint();
-			}
+	public void finishGame() {
+		gameInactive = true;
+		highScore.add(guessCount);	
+		System.out.println(String.format("GameOver! It took you %d guesses.", guessCount));
+		for(Tile tile: tileList) {
+			tile.open();
 		}
+		
+	}
+	
+	public void lineReader(String file) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(new File(file)));
+			String line = null;
+			while((line = reader.readLine()) != null) {
+				System.out.println(line);
+			} 
+			reader.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void lineWriter(String line, String file) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(file)));
+			for(int score: highScore) {
+				writer.write(score + "\n");
+			}
+			writer.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public class highScoresListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			lineReader("highscore.txt");
+		}//method
 	}//inner
 
 	class Background extends JPanel {
@@ -79,21 +134,31 @@ public class BOI {
 		}
 	}//inner
 
-	class Tile extends JPanel {
-		boolean isShip = false;
-		boolean isChecked = false;
-		Point XY;
+	public class Tile extends JPanel {
+		boolean isChecked;
+		boolean hasShip;
+		Point location;
 
-		Color field = Color.decode("#5b5266");
+		Color field = Color.decode("#5B5266");
 		Color ship = Color.decode("#FF6565");
 		Color checked = Color.decode("#2D4C81");
 
 		public Tile(int x, int y) {
-			XY = new Point(x, y);
+			location = new Point(x, y);
+		}
+		public void reset() {
+			isChecked = false;
+			hasShip = false;
+			repaint();
+		}
+		
+		public void open() {
+			isChecked = true;
+			repaint();
 		}
 
-		public void isShip() {
-			isShip = true;
+		public void hasShip() {
+			hasShip = true;
 		}
 
 		public void isChecked() {
@@ -102,42 +167,128 @@ public class BOI {
 
 		public void paintComponent(Graphics g) {
 			Graphics2D g2d = (Graphics2D) g;
-			if(!isChecked) {
-				g2d.setColor(field);
-				g2d.fillRect(-0, 0, this.getWidth(), this.getHeight());
-			} else {
-				if(isShip) {
+			if(isChecked) {
+				if(hasShip) {
 					g2d.setColor(ship);
 					g2d.fillRect(-0, 0, this.getWidth(), this.getHeight());
 				} else {
 					g2d.setColor(checked);
 					g2d.fillRect(-0, 0, this.getWidth(), this.getHeight());
 				}
-			}//else
+			} else {
+				g2d.setColor(field);
+				g2d.fillRect(-0, 0, this.getWidth(), this.getHeight());
+			}
 		}//paint
+	}//inner
+
+	public class RestartListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			gameInactive = true;
+			guessCount = 0;
+			Generator.makeMap(MAP_X, MAP_Y, SHIP_N);
+			shipList = Generator.getShipList();
+			for (Tile tile: tileList) {
+				tile.reset();
+				for(Ship ship: shipList) {
+					if(ship.cells.contains(tile.location)) {
+						tile.hasShip();
+						break;
+					}
+				}//for ship
+			}//for tile
+			gameInactive = false;
+		}//method
+	}//class
+
+	public class GuessListener extends MouseAdapter implements MouseListener {
+		public void mouseReleased(MouseEvent event) {
+			String result = "miss";
+			if(!gameInactive) {
+				Tile t = (Tile)(event.getSource());
+				if(t.isChecked == false) {
+					guessCount++;
+					t.isChecked();
+					t.repaint();
+
+					for(Ship ship: shipList) {
+						result = ship.hitCheck(t.location);
+						if(result.equals("hit")) {
+							break;
+						}
+						if(result.equals("kill")) {
+							shipList.remove(ship);
+							break;
+						}
+					}
+					System.out.println(result);
+
+					if(shipList.isEmpty()) {
+						finishGame();
+					}
+				}//if
+			}//if active
+		}//method
 	}//inner
 }//outer
 
-abstract class Generator {				
-	private static ArrayList<Ship> shipList = new ArrayList<Ship>();
+class Ship {
+	ArrayList<Point> cells;
 	
+	public Ship(int vH, int size, int startX, int startY) {
+		int x;
+		int y; 
+		cells = new ArrayList<Point>();
+		System.out.println("Placed ship at:");
+		for(int i = 0; i < size; i++) {
+			x = startX + i*(1-vH);
+			y = startY + i*vH;
+			System.out.println(String.format("(%d, %d)", x, y));
+			cells.add(new Point(x, y));
+		}
+	}//method
+
+	public String hitCheck(Point p) {
+		String result = "miss";
+		if(cells.contains(p)) {
+			cells.remove(p);
+			if(cells.isEmpty() != true) {
+				result = "hit";
+			} else {
+				result = "kill";
+			}
+		}
+		return result;
+	}//method
+}
+
+final class Generator {				
+	private static ArrayList<Ship> shipList = new ArrayList<Ship>();
+	private static int failureCount = 0;
+
+	private Generator() { }
+
 	public static ArrayList<Ship> getShipList() {
 		return shipList;
 	}
 
 	public static void makeMap(int mapX, int mapY, int n) {		//generates 1xN, 2x(N-1),.. , Nx1 ships on (MAP_X, MAP_Y) map
+		failureCount = 0;
+
+		shipList.clear();
 		boolean startAnew = true;
 
 		while(startAnew) {
 			shipList.clear();
+			outerloop:	
 			for(int i = n; i > 0; i--) {							//iterates through ship size
 				for(int j = i - 1; j < n; j++) {					//iterates through number of ships
 					startAnew = placeShip(mapX, mapY, i);
-					if(startAnew) {break;}
+					if(startAnew) {break outerloop;}
 				}
-				if(startAnew) {break;}
 			}//for i
 		}//while
+		System.out.println("Generator.MakeMap() successful.");
 	}//method
 
 	public static boolean placeShip(int mapX, int mapY, int size) {
@@ -146,7 +297,7 @@ abstract class Generator {
 		boolean spare = true;
 		boolean failure = false;
 
-		int vH;					//ship orientation, 1 for vertical, 0 for horisontal
+		int vH = 0;					//ship orientation, 1 for vertical, 0 for horisontal
 		int startX = 0;
 		int startY = 0;
 
@@ -172,44 +323,14 @@ abstract class Generator {
 			if(spare) {
 				shipList.add(new Ship(vH, size, startX, startY));
 				placed = true;
-				System.out.println(String.format("\nvH = %d ship of size %d starting at (%d, %d)\n", vH, size, startX, startY));
 			}
 			
-			if(count++ > 200) {
+			if(count++ > (BOI.MAP_X * BOI.MAP_Y * 2)) {
 				failure = true;
-				System.out.println(String.format("\nGENERATION FAILED at 200 steps, starting anew..\n"));
+				System.out.println(String.format("\nGENERATION FAILED at %d step of attempt %d, starting anew..\n", count, ++failureCount));
 				break;
 			}
 		}//while
 		return failure;
 	}//method
 }//class
-
-class Ship {
-	ArrayList<Point> cells;
-	
-	public Ship(int vH, int size, int startX, int startY) {
-		int x;
-		int y; 
-		cells = new ArrayList<Point>();
-		System.out.println("Placed ship at:");
-		for(int i = 0; i < size; i++) {
-			x = startX + i*(1-vH);
-			y = startY + i*vH;
-			System.out.println(String.format("(%d, %d)", x, y));
-			cells.add(new Point(x, y));
-		}
-
-	}//method
-
-	public boolean isIn(Point p) {
-		boolean isIn = false;
-		for(Point cell: cells) {
-			if(cell.equals(p)) {
-				isIn = true;
-			}
-		}
-		return isIn;	
-	}//method
-}
-
--- hfj/Quiz.java
+++ hfj/Quiz.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 public class Quiz {
 	JFrame startScreen;
@@ -28,35 +29,88 @@ public class Quiz {
 }
 
 class QuizCardBuilder {
-	public void callGui() {
-	//make:
-	//1. A frame with two JTextArea
-	//for q/a input
-	//2. JButton "Next Card"
-	//adds a card to card list
-	//3.File>Save menu
-	//to save the cards in list to file
-	//4.File>New
-	//to make a new set
+	private JTextArea question;
+	private JTextArea answer;
+	private ArrayList<QuizCard> cardList;
+	private JFrame frame;
+
+	public void go() {
+		frame = new JFrame(“Quiz Card Builder”);
+		JPanel mainPanel = new JPanel();
+		Font bigFont = new Font(“sanserif”, Font.BOLD, 24);
+		question = new JTextArea(6,20);
+		question.setLineWrap(true);
+		question.setWrapStyleWord(true);
+		question.setFont(bigFont);
+
+		JScrollPane qScroller = new JScrollPane(question);
+		qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		answer = new JTextArea(6,20);
+		answer.setLineWrap(true);
+		answer.setWrapStyleWord(true);
+		answer.setFont(bigFont);
+		
+		JScrollPane aScroller = new JScrollPane(answer);
+		aScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		aScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		JButton nextButton = new JButton(“Next Card”);
+		
+		cardList = new ArrayList<QuizCard>();
+
+		JLabel qLabel = new JLabel(“Question:”);
+		JLabel aLabel = new JLabel(“Answer:”);
+
+		mainPanel.add(qLabel);
+		mainPanel.add(qScroller);
+		mainPanel.add(aLabel);
+		mainPanel.add(aScroller);
+		mainPanel.add(nextButton);
+		nextButton.addActionListener(new NextCardListener());
+
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem newMenuItem = new JMenuItem("New");
+		JMenuItem saveMenuItem = new JMenuItem("Save");
+		
+		newMenuItem.addActionListener(new NewMenuListener());
+		saveMenuItem.addActionListener(new SaveMenuListener());
+		
+		fileMenu.add(newMenuItem);
+		fileMenu.add(saveMenuItem);
+		menuBar.add(fileMenu);
+		frame.setJMenuBar(menuBar);
+		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+		frame.setSize(500,600);
+		frame.setResizable(false);
+		frame.setVisible(true);
 	}
-	//inner listener classes
+
 	class NextCardListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			//add current card to a list
 			//make next card, clear text fields
 		}
 	}
-	
-	class NewListener implements ActionListener {
+		
+	class SaveMenuListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			//make new card set
+			//bring up a file dialog box to name the set and save it
+			//save card set to a file using saveFile()
 		}
 	}
-	
-	class SaveListener implements ActionListener {
+
+	class NewMenuListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			//save card set to a file
+			//clear q/a text fields and card list to make a new card set
 		}
+	}
+
+	private void saveFile(File file) {
+		//iterate through the list of cards
+		//write each to a text file in a parseable way
 	}
 }
 
